79e327846c2280562f427fe8cdc8d3da69b30e85
wildrun0 <gamesnouth@yandex.ru>
2022-09-30 22:38:19 +0500

    URL REGEX added to commands url recognition added code styling improvement (i hope so) who.py command added

--- methods/get_command_arguments.py
+++ methods/get_command_arguments.py
@@ -1,8 +1,12 @@
 import re
 from aiocache import cached
+
 from datatypes import User, PeerObject
 from datatypes.user import set_user, get_user
-from settings.bot_commands import UID_REGEX
+
+from settings.bot_commands import UID_REGEX, URL_UID_REGEX
+from settings.config import BOT_ID
+from loader import bot
 
 
 @cached(ttl=300)
@@ -14,15 +18,33 @@ async def get_command_arguments(
 ) -> tuple[str, tuple[User|None, list[str]]]:
     for command in regex_list:
         if matches := re.findall(command, msg_candidate):
-            args = list(*matches) if isinstance(*matches, tuple) else matches
-            if UID_REGEX in command:
+            raw_args = list(*matches) if isinstance(*matches, tuple) else matches
+            args = list(filter(None, map(str.strip, raw_args))) # removing blank strings in list
+            print(args)
+            if UID_REGEX in command or URL_UID_REGEX in command:
                 try:
-                    id = -int(args[1]) if args[0] == "club" else int(args[1])
-                    if id in peer_object.data.users:
+                    if re.search(URL_UID_REGEX, msg_candidate) and (
+                        (str_id := args[0]) not in ["club", 'id']
+                    ):
+                        try:
+                            id = (await bot.api.users.get(
+                                user_ids=str_id
+                            ))[0].id
+                        except IndexError:
+                            id = -(await bot.api.groups.get_by_id(
+                                group_id=str_id
+                            ))[0].id
+                        args.insert(0, '')
+                        # чтобы соответсвовать коду ниже, где args имеют вид (0,1,2,3,4)
+                    else:
+                        id = -int(args[1]) if args[0] == "club" else int(args[1])
+                    if id in peer_object.data.users and id != BOT_ID:
                         args = (await get_user(id, peer_object.peer_id), args[2:])
                     else:
                         args = (None, args[2:])
-                except ValueError: pass
+                except (ValueError, IndexError):
+                    args.insert(0, '')
+                    pass
             else:       # хз кто ета
                 if user_id in peer_object.data.users: 
                     user = await get_user(user_id, peer_object.peer_id)
